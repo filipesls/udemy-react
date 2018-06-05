13e718fcadf82a31292c6929bfbcf7c3653f3496
Filipe Santana <filipe.santana@gmail.com>
2018-06-05 21:42:09 +0100

    stateless function component
    Change the other components

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -62,15 +62,13 @@ class IndecisionApp extends React.Component {
     }
 }
 
-class Header extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>{this.props.title}</h1>
-                <h2>{this.props.subtitle}</h2>
-            </div>
-        );
-    }
+const Header = (props) => {
+    return (
+        <div>
+            <h1>{props.title}</h1>
+            <h2>{props.subtitle}</h2>
+        </div>
+    );
 }
 
 const Action = (props) => {
@@ -86,28 +84,24 @@ const Action = (props) => {
     );
 }
 
-class Options extends React.Component {
-    render() {
-        return (
-            <div>
-                <button onClick={this.props.handleDeleteOptions}>Remove all!</button>
-                {
-                    this.props.options.map((option) => <Option key={option} optionText={option} />)
-                    
-                }
-            </div>
-        );
-    }
+const Options = (props) => {
+    return (
+        <div>
+            <button onClick={props.handleDeleteOptions}>Remove all!</button>
+            {
+                props.options.map((option) => <Option key={option} optionText={option} />)
+
+            }
+        </div>
+    )
 }
 
-class Option extends React.Component {
-    render() {
-        return (
-            <div>
-                Option: {this.props.optionText}
-            </div>
-        );
-    }
+const Option = (props) => {
+    return (
+        <div>
+            Option: {props.optionText}
+        </div>
+    );
 }
 
 class AddOption extends React.Component {
