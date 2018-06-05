ee99535d162fd010dfa22559924333f6752e9c1d
Filipe Santana <filipe.santana@gmail.com>
2018-06-05 20:05:13 +0100

    Section 4 - React Components

---
+++ indecision-app/src/playground/es6-classes-1.js
@@ -0,0 +1,71 @@
+
+class Person {
+    constructor(name = 'Anonymous', age = '0') {
+        this.name = name;    
+        this.age = age    
+    }
+
+    getGretting() {
+        return `Hi, I am ${this.name}!`
+    }
+
+    getDescription() {
+        return `${this.name} is ${this.age} year(s) old!`
+    }
+}
+
+class Student extends Person {
+    constructor(name, age, major) {
+        super(name, age);
+        this.major = major;
+    }
+
+    hasMajor() {
+        return !!this.major;
+    }
+
+    getDescription() {
+        let description = super.getDescription();
+
+        if (this.hasMajor()) {
+            description += ` Their major is ${this.major}.`;
+        }
+        return description
+    }
+}
+
+class Traveler extends Person {
+    constructor(name, age, homeLocation) {
+        super(name, age);
+        this.homeLocation = homeLocation;
+    }
+
+    getGreeting() {
+        let greeting = super.getGretting();
+
+        if (this.homeLocation) {
+            greeting += ` I'm visiting from ${this.homeLocation}.`;
+        }
+
+        return greeting;
+    }
+}
+
+const me = new Traveler('Filipe Santana', 35, 'São Paulo');
+console.log(me.getGreeting());
+
+const other = new Traveler(undefined, undefined, 'Nowhere');
+console.log(other.getGreeting());
+
+
+// const me = new Student('Filipe Santana', 35, 'Computer Science');
+// console.log(me);
+// console.log(me.getGretting());
+// console.log(me.getDescription());
+// console.log(me.hasMajor());
+
+// const other = new Student();
+// console.log(other);
+// console.log(other.getGretting());
+// console.log(other.getDescription());
+// console.log(other.hasMajor());
