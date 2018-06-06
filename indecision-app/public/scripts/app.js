19f8ec2fd1aac7c33a5d6a14b1781063e7a44ab8
Filipe Santana <filipe.santana@gmail.com>
2018-06-06 16:12:45 +0100

    reducing the size of code

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -50,9 +50,7 @@ var IndecisionApp = function (_React$Component) {
             }
 
             this.setState(function (prevState) {
-                return {
-                    options: prevState.options.concat(option)
-                };
+                return { options: prevState.options.concat(option) };
             });
         }
     }, {
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -28,11 +28,7 @@ class IndecisionApp extends React.Component {
             return 'This option already exists';
         }
 
-        this.setState((prevState) => {
-            return {
-                options: prevState.options.concat(option)
-            };
-        });
+        this.setState((prevState) => ({ options: prevState.options.concat(option) }));
     }
 
     render() {
@@ -121,9 +117,7 @@ class AddOption extends React.Component {
         const option = e.target.elements.option.value.trim();
         const error = this.props.handleAddOption(option);
 
-        this.setState(() => {
-            return { error };
-        });
+        this.setState(() => ({ error }));
     }
 
     render() {
