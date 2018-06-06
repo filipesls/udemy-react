873084a4ce1b34538ead87c2e769319fd5b9b326
Filipe Santana <filipe.santana@gmail.com>
2018-06-07 00:59:50 +0100

    ES6 Import Export
    import export named

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,1 +1,11 @@
+// import './utils.js';
+import { square, add } from './utils.js'
+import { isAdult, canDrink } from './person.js'
+
 console.log('App.js is running');
+
+console.log(square(4));
+console.log(add(100, 23));
+
+console.log(isAdult(18));
+console.log(canDrink(18));
