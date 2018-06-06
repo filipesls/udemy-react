1bb1d42c59533fbf3ad407bf567da18d1b43abc8
Filipe Santana <filipe.santana@gmail.com>
2018-06-06 18:32:04 +0100

    Lyfecycle methods && Saving and Loading Options data, using localStorage
    keep the information on the screen

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -29,7 +29,49 @@ var IndecisionApp = function (_React$Component) {
         return _this;
     }
 
+    // lifecycle Methods
+
+
     _createClass(IndecisionApp, [{
+        key: 'componentDidMount',
+        value: function componentDidMount() {
+            try {
+                var json = localStorage.getItem('options');
+                var options = JSON.parse(json);
+
+                if (options) {
+                    this.setState(function () {
+                        return { options: options };
+                    });
+                }
+            } catch (error) {
+                // Do nothing at all
+            }
+        }
+
+        // lifecycle Methods
+
+    }, {
+        key: 'componentDidUpdate',
+        value: function componentDidUpdate(prevProps, prevState) {
+            {
+                if (prevState.options.length !== this.state.options.length) {
+                    var json = JSON.stringify(this.state.options);
+                    localStorage.setItem('options', json);
+                }
+            }
+        }
+
+        // lifecycle Methods
+
+    }, {
+        key: 'componentWillUnmount',
+        value: function componentWillUnmount() {
+            {
+                console.log('componentWillUnmount');
+            }
+        }
+    }, {
         key: 'handleDeleteOptions',
         value: function handleDeleteOptions() {
             this.setState(function () {
@@ -146,6 +188,11 @@ var Options = function Options(props) {
             { onClick: props.handleDeleteOptions },
             'Remove All'
         ),
+        props.options.length === 0 && React.createElement(
+            'p',
+            null,
+            'Please add an option to get started!'
+        ),
         props.options.map(function (option) {
             return React.createElement(Option, {
                 key: option,
@@ -200,7 +247,9 @@ var AddOption = function (_React$Component2) {
                 return { error: error };
             });
 
-            e.target.elements.option.value = '';
+            if (!error) {
+                e.target.elements.option.value = '';
+            }
         }
     }, {
         key: 'render',
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -14,6 +14,33 @@ class IndecisionApp extends React.Component {
         this.handleAddOption = this.handleAddOption.bind(this);
     }
 
+    // lifecycle Methods
+    componentDidMount() {
+        try {
+            const json = localStorage.getItem('options');
+            const options = JSON.parse(json);
+
+            if (options) {
+                this.setState(() => ({ options: options }));
+            }
+        } catch (error) {
+            // Do nothing at all
+        }
+    }
+
+    // lifecycle Methods
+    componentDidUpdate(prevProps, prevState) {{
+        if (prevState.options.length !== this.state.options.length) {
+            const json = JSON.stringify(this.state.options);
+            localStorage.setItem('options', json)
+        }
+    }}
+
+    // lifecycle Methods
+    componentWillUnmount() {{
+        console.log('componentWillUnmount')
+    }}
+
     handleDeleteOptions() {
         this.setState(() => ({ options: [] }));
     }
@@ -99,6 +126,7 @@ const Options = (props) => {
     return (
         <div>
             <button onClick={props.handleDeleteOptions}>Remove All</button>
+            {props.options.length === 0 && <p>Please add an option to get started!</p>}
             {
                 props.options.map((option) => (
                     <Option
@@ -144,7 +172,9 @@ class AddOption extends React.Component {
 
         this.setState(() => ({ error }));
 
-        e.target.elements.option.value = '';
+        if (!error) {
+            e.target.elements.option.value = '';
+        }
     }
 
     render() {
