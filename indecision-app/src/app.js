4c98f627b00b58df12f19c6371c53f794d730407
Filipe Santana <filipe.santana@gmail.com>
2018-06-08 00:25:24 +0100

    removing example

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -3,34 +3,3 @@ import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp'
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
-
-class OldSyntax {
-    constructor() {
-        this.name = 'Mike';
-        this.getGreeting = this.getGreeting.bind(this);
-    }
-    getGreeting() {
-        return `Hi, my name is ${this.name}.`;
-    }
-}
-const oldSyntax = new OldSyntax();
-const getGreeting = oldSyntax.getGreeting;
-console.log(getGreeting());
-
-/* ----------
-babeljs.io - plugin - transform-class-properties in .babelrc
-https://babeljs.io/docs/plugins/transform-class-properties/
-
-not necessary to use contructor() and bind the function
----------- */
-
-class NewSyntax {
-    name = 'Jen';
-    getGreeting = () => {
-        return `Hi, my name is ${this.name}.`;
-    }
-}
-
-const newSyntax = new NewSyntax();
-const newGetGreeting = newSyntax.getGreeting;
-console.log(newGetGreeting());
