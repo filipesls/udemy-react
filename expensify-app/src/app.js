d36eb6dd1c56a5b1e5dd231783bbcf8b25f20db0
Filipe Santana <filipe.santana@gmail.com>
2018-06-11 00:56:08 +0100

    Router Settthing up - basic

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,14 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom'
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-class BoilerplateApp extends React.Component{
-    render(){
-        return (
-            <p>This is my boilerplate</p>
-        )
-    }
-}
+const ExpensiveDashboardPage = () => (
+    <div>
+        This is from my dashboard component
+    </div>
+);
 
-ReactDOM.render(<BoilerplateApp />, document.getElementById('app'));
+const AddExpensivePage = () => (
+    <div>
+        This is from my add expensive component
+    </div>
+);
+
+const EditExpensivePage = () => (
+    <div>
+        This is from my edit expensive component
+    </div>
+);
+
+const HelpPage = () => (
+    <div>
+        This is from my help page component
+    </div>
+);
+
+const routes =(
+    <BrowserRouter>
+        <div>
+            <Route exact path="/" component={ExpensiveDashboardPage} />
+            <Route path="/create" component={AddExpensivePage} />
+            <Route path="/edit" component={EditExpensivePage} />
+            <Route path="/help" component={HelpPage} />
+        </div>
+    </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
