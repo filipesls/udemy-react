25ee45afe57fe216a603cac981caae1785083c12
Filipe Santana <filipe.santana@gmail.com>
2018-06-11 01:03:01 +0100

    Router setting up - 404 page

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom'
+import {
+    BrowserRouter,
+    Route,
+    Switch
+} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
@@ -28,14 +32,21 @@ const HelpPage = () => (
     </div>
 );
 
+const NotFoundPage = () => (
+    <div>
+        404!
+    </div>
+);
+
 const routes =(
     <BrowserRouter>
-        <div>
+        <Switch>
             <Route exact path="/" component={ExpensiveDashboardPage} />
             <Route path="/create" component={AddExpensivePage} />
             <Route path="/edit" component={EditExpensivePage} />
             <Route path="/help" component={HelpPage} />
-        </div>
+            <Route component={NotFoundPage} />
+        </Switch>
     </BrowserRouter>
 );
 
