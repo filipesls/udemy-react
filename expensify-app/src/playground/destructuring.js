f9b6d137f199bb1e59418c19b4148b829f22c25d
Filipe Santana <filipe.santana@gmail.com>
2018-06-11 20:33:52 +0100

    ES6 Object Destructuring

---
+++ expensify-app/src/playground/destructuring.js
@@ -0,0 +1,29 @@
+// const person = {
+//     name: 'Filipe',
+//     age: 35,
+//     location: {
+//         city: 'Dublin',
+//         temp: 18
+//     }
+// }
+
+// const { name: firstName = 'Anonymous', age } = person;
+// console.log(`${firstName} is ${age}.`);
+
+// const { city, temp: temperature } = person.location;
+
+// if (city && temperature) {
+//     console.log(`It's ${temperature} in ${city}.`);
+// }
+
+const book = {
+    title: 'Ego is the Enemy',
+    author: 'Ryan Holiday',
+    publisher: {
+        name: 'Penguin'
+    }
+}
+
+const { name: publisherName = 'Self-Publisher' } = book.publisher;
+
+console.log(publisherName); //Penguin, Self-Publisher
