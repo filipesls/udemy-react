471d332ff22ad918d69b88aa3806da00e39c0a52
Filipe Santana <filipe.santana@gmail.com>
2018-06-12 17:31:15 +0100

    ES6 Spread Operator in Reducers

--- expensify-app/src/playground/redux-expensify.js
+++ expensify-app/src/playground/redux-expensify.js
@@ -1,7 +1,24 @@
 import { createStore, combineReducers } from 'redux';
+import uuid from 'uuid';
 
 // ADD_EXPENSE
+const addExpense = ({ description = '', note = '', amount = 0, createdAt = 0 } = {}) => ({
+    type: 'ADD_EXPENSE',
+    expense: {
+        id: uuid(),
+        description,
+        note,
+        amount,
+        createdAt
+    }
+});
+
 // REMOVE_EXPENSE
+const removeExpense = ({ id } = {}) => ({
+    type: 'REMOVE_EXPENSE',
+    id
+});
+
 // EDIT_EXPENSE
 // SET_TEXT_FILTER
 // SORT_BY_DATE
@@ -13,6 +30,12 @@ import { createStore, combineReducers } from 'redux';
 const expensesReducerDefaultState = [];
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
     switch (action.type) {
+        case 'ADD_EXPENSE':
+            return [ ...state, action.expense ]
+            break;
+        case 'REMOVE_EXPENSE':
+            return state.filter(({ id }) => id !== action.id);
+            break;
         default:
             return state;
             break;
@@ -20,7 +43,12 @@ const expensesReducer = (state = expensesReducerDefaultState, action) => {
 }
 
 // Filters Reducer
-const filtersReducerDefaultState = { text: '', sortBy: 'date', startDate: undefined, endDate: undefined };
+const filtersReducerDefaultState = {
+    text: '',
+    sortBy: 'date',
+    startDate: undefined,
+    endDate: undefined
+};
 const filtersReducer = (state = filtersReducerDefaultState, action) => {
     switch (action.type) {
         default:
@@ -36,8 +64,17 @@ const store = createStore(
         filters: filtersReducer
     })
 );
-console.log(store.getState());
 
+store.subscribe(() => {
+    console.log(store.getState());
+});
+
+const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100 }));
+const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 300 }));
+
+store.dispatch(removeExpense({ id: expenseOne.expense.id }))
+
+console.log(expenseOne);
 
 const demoState = {
     expenses: [{
