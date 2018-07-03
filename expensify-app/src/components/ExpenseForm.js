c6f43bd3ba0c3459ce9601039f267360f6d9cf26
Filipe Santana <filipe.santana@gmail.com>
2018-07-03 23:43:54 +0100

    Wiring up Edit Expense

--- expensify-app/src/components/ExpenseForm.js
+++ expensify-app/src/components/ExpenseForm.js
@@ -6,13 +6,17 @@ import 'react-dates/initialize';
 import 'react-dates/lib/css/_datepicker.css';
 
 class ExpenseForm extends React.Component {
-    state = {
-        description: '',
-        amount: '',
-        note: '',
-        createdAt: moment(),
-        calendarFocused: false
-    };
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            description: props.expense ? props.expense.description : '',
+            note: props.expense ? props.expense.note : '',
+            amount: props.expense ? (props.expense.amount / 100).toString() : '',
+            createdAt: props.expense ? moment(props.expense.createdAt) : moment(),
+            calendarFocused: false
+        };
+    }
 
     onDescriptionChange = (e) => {
         const description = e.target.value;
--- expensify-app/src/components/ExpenseListItem.js
+++ expensify-app/src/components/ExpenseListItem.js
@@ -1,27 +1,14 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { removeExpense } from '../actions/expenses';
+import { Link } from 'react-router-dom';
 
 /* Using Props - My way */
 const ExpenseListItem = (props) => (
     <div>
-        <h3>{props.description}</h3>
+        <Link to={`/edit/${props.id}`}>
+            <h3>{props.description}</h3>
+        </Link>
         <p>{props.amount} - {props.createdAt}</p>
-        <button onClick={(e) => {
-            props.dispatch(removeExpense({ id: props.id }));
-        }}>Remove</button>
     </div>
 );
 
-/* Using Object - How they did on video */
-// const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => (
-//     <div>
-//         <h3>{description}</h3>
-//         <p>{amount} - {createdAt}</p>
-//         <button onClick={(e) => {
-//             dispatch(removeExpense({ id }));
-//         }}>Remove</button>
-//     </div>
-// );
-
-export default connect()(ExpenseListItem);
+export default ExpenseListItem;
