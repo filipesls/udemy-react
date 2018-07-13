938f71d892e460d6b7ed5e82412285a8531c75cb
Filipe Santana <filipe.santana@gmail.com>
2018-07-13 02:22:45 +0100

    Change the dispatch, using mapDispatchToProps

--- expensify-app/src/components/AddExpensivePage.js
+++ expensify-app/src/components/AddExpensivePage.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
 import { addExpense} from '../actions/expenses';
 
-class AddExpensivePage extends React.Component {
+export class AddExpensivePage extends React.Component {
     onSubmit = (expense) => {
         this.props.onSubmit(expense);
         this.props.history.push('/');
