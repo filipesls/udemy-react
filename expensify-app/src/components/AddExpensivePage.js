189b89b0ea38173ce0a2f8150100311460b4d4ec
Filipe Santana <filipe.santana@gmail.com>
2018-07-13 02:21:57 +0100

    Change the dispatch, using mapDispatchToProps

--- expensify-app/src/components/AddExpensivePage.js
+++ expensify-app/src/components/AddExpensivePage.js
@@ -3,20 +3,25 @@ import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
 import { addExpense} from '../actions/expenses';
 
-const AddExpensivePage = (props) => (
-    <div>
-        <h1>Add Expense</h1>
-        <ExpenseForm 
-            onSubmit={(expense) => {
-                props.onSubmit(expense);
-                props.history.push('/');
-            }}
-        />
-    </div>
-);
+class AddExpensivePage extends React.Component {
+    onSubmit = (expense) => {
+        this.props.onSubmit(expense);
+        this.props.history.push('/');
+    }
+    render() {
+        return (
+            <div>
+                <h1>Add Expense</h1>
+                <ExpenseForm
+                    onSubmit={this.onSubmit}
+                />
+            </div>
+        );
+    }
+}
 
 const mapDispatchToProps = (dispatch) => ({
     onSubmit: (expense) => dispatch(addExpense(expense))
-})
+});
 
 export default connect(undefined, mapDispatchToProps)(AddExpensivePage);
