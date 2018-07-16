1533fe73ac792ece748052c32f352616572913a9
Filipe Santana <filipe.santana@gmail.com>
2018-07-16 17:09:56 +0100

    Testing ExpenseListFilters

--- expensify-app/src/__tests__/components/ExpenseListFilters.test.js
+++ expensify-app/src/__tests__/components/ExpenseListFilters.test.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import moment from 'moment';
 import { shallow } from 'enzyme';
 import { ExpenseListFilters } from '../../components/ExpenseListFilters';
 import { filters, altFilters } from '../../__mocks__/filters';
 
-let setTextFilterSpy, sortByDateSpy, SortByAmountSpy, setStartDateSpy, setEndDateSpy, wrapper;
+let setTextFilterSpy, sortByDateSpy, sortByAmountSpy, setStartDateSpy, setEndDateSpy, wrapper;
 
 beforeEach(() => {
     setTextFilterSpy = jest.fn();
     sortByDateSpy = jest.fn();
-    SortByAmountSpy = jest.fn();
+    sortByAmountSpy = jest.fn();
     setStartDateSpy = jest.fn();
     setEndDateSpy = jest.fn();
     wrapper = shallow(
@@ -16,7 +17,7 @@ beforeEach(() => {
             filters={filters}
             setTextFilter={setTextFilterSpy}
             sortByDate={sortByDateSpy}
-            SortByAmount={SortByAmountSpy}
+            sortByAmount={sortByAmountSpy}
             setStartDate={setStartDateSpy}
             setEndDate={setEndDateSpy}
         />
@@ -34,3 +35,43 @@ test('should render ExpenseListFilters with alt data correctly', () => {
     expect(wrapper).toMatchSnapshot();
 });
 
+test('should handle text change', () => {
+    const value = 'rent';
+    wrapper.find('input').simulate('change', {
+        target: { value }
+    });
+    expect(setTextFilterSpy).toHaveBeenLastCalledWith(value);
+});
+
+test('should sort by date', () => {
+    const value = 'date';
+    wrapper.setProps({
+        filters: altFilters
+    });
+    wrapper.find('select').simulate('change', {
+        target: { value }
+    });
+    expect(sortByDateSpy).toHaveBeenCalled();
+});
+
+test('should sort by amount', () => {
+    const value = 'amount';
+    wrapper.find('select').simulate('change', {
+        target: { value }
+    });
+    expect(sortByAmountSpy).toHaveBeenCalled();
+});
+
+test('should handle date changes', () => {
+    const startDate = moment().add(4, 'years');
+    const endDate = moment().add(8, 'years');
+    wrapper.find('withStyles(DateRangePicker)').prop('onDatesChange')({ startDate, endDate });
+    expect(setStartDateSpy).toHaveBeenLastCalledWith(startDate);
+    expect(setEndDateSpy).toHaveBeenLastCalledWith(endDate);
+});
+
+test('should handle date  focus changes', () => {
+    const calendarFocused = 'endDate';
+    wrapper.find('withStyles(DateRangePicker)').prop('onFocusChange')(calendarFocused);
+    expect(wrapper.state('calendarFocused')).toBe(calendarFocused);
+});
