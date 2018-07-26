39af67476db15bd33f04503a3961bc6f33d8f95f
Filipe Santana <filipe.santana@gmail.com>
2018-07-26 19:27:46 +0100

    152. Asynchronous Redux Actions

--- expensify-app/src/firebase/firebase.js
+++ expensify-app/src/firebase/firebase.js
@@ -13,20 +13,22 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 
-// child_removed
-database.ref('expenses').on('child_removed', (snapshot) => {
-    console.log('This expense was removed - ', snapshot.key, snapshot.val());
-});
+export { firebase, database as default };
 
-// child_changed
-database.ref('expenses').on('child_changed', (snapshot) => {
-    console.log('This expense was changed - ', snapshot.key, snapshot.val());
-});
+// // child_removed
+// database.ref('expenses').on('child_removed', (snapshot) => {
+//     console.log('This expense was removed - ', snapshot.key, snapshot.val());
+// });
 
-// child_added
-database.ref('expenses').on('child_added', (snapshot) => {
-    console.log('This expense was added - ', snapshot.key, snapshot.val());
-});
+// // child_changed
+// database.ref('expenses').on('child_changed', (snapshot) => {
+//     console.log('This expense was changed - ', snapshot.key, snapshot.val());
+// });
+
+// // child_added
+// database.ref('expenses').on('child_added', (snapshot) => {
+//     console.log('This expense was added - ', snapshot.key, snapshot.val());
+// });
 
 // database.ref('expenses').on('value', (snapshot) => {
 //     const expenses = [];
